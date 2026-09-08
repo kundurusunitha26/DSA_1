c26f4d4276d3e5eb65828cf8bf9695e4c501548e
kundurusunitha26 <kundurusunitha085@gmail.com>
2026-09-08 14:03:03 +0530

    Initial commit

---
+++ src/Demo/UnsortedBinarySearch.java
@@ -0,0 +1,36 @@
+package Demo;
+
+import java.util.Arrays;
+
+public class UnsortedBinarySearch {
+		static int binarySearch(int[] arr,int key) {
+			int low = 0;
+			int high = arr.length-1;
+			while(low <= high) {
+				int mid = low + (high - low) / 2;
+				if (arr[mid] == key) {
+					return mid;
+				}
+				if (arr[mid] < key) {
+					low = mid + 1;
+				} else {
+					high = mid - 1;
+				}
+			}
+			return -1;
+		}
+		public static void main(String[] args) {
+			int[] arr = {50,10,40,20,70,30,60};
+			int key = 40;
+			Arrays.sort(arr);
+			int result = binarySearch(arr, key);
+			if (result != -1)
+				System.out.println("Element: " + key +" Found at index: " + result);
+			else
+				System.out.println("Element" + key + "not found");
+		}
+
+	}
+
+
+
