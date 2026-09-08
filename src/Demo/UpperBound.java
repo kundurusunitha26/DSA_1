c0dfff90ec6b3da45069e84489369d79dc0e24ba
kundurusunitha26 <kundurusunitha085@gmail.com>
2026-09-08 14:13:02 +0530

    UpperBound

---
+++ src/Demo/UpperBound.java
@@ -0,0 +1,27 @@
+package Demo;
+
+public class UpperBound {
+		static int upperBound(int[] arr, int target) {
+			int left = 0;
+			int right = arr.length;
+			while (left < right) {
+				int mid = left + (right - left) / 2;
+				if (arr[mid] > target) {
+					right = mid;
+				} else {
+					left = mid + 1;
+				}
+			}
+			return left;
+		}
+		public static void main(String[] args) {
+			int[] arr = {10,20,20,20,30,40,50};
+			int target = 50;
+			int index = upperBound(arr, target);
+			System.out.println("Upper Bound: " + index);
+			
+		}
+
+	}
+
+
